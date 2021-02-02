cda750b0adead863ff97bc345232cac4953684b7
Vinaykukati <vinaykukati121@gmail.com>
2021-02-02 16:06:41 +0530

    5uhy

--- thirdproject/src/thirdproject/samplejava3.java
+++ thirdproject/src/thirdproject/samplejava3.java
@@ -2,6 +2,6 @@ package thirdproject;
 
 public class samplejava3 {
 	public static void main(String args[]) {
-		System.out.println("This is sample third java example");
+		System.out.println("This is sample third java example..");
 	}
 }
