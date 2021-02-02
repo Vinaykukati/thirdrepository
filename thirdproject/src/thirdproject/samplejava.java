604f0e85bd3f2481f4ff8c6d19377b074a1ed534
vinaykukati <vinaykukati121@gmail.com>
2021-02-03 00:37:24 +0530

    Jenkins commit

---
+++ thirdproject/src/thirdproject/samplejava.java
@@ -0,0 +1,10 @@
+package firstproj;
+
+public class samplejava {
+	public static void main(String args[]){
+		System.out.println("This is test program121");
+		System.out.println("This is second line firstbranch123");
+		System.out.println("This is second line by firstbranc2");
+		System.out.println("This is second line firstbranch");
+	}
+}
